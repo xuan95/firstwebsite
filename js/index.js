8e57ee3df392f6d4286ff39ac4faf344eac01c50
Xuan <xhanhnv@gmail.com>
2016-05-22 13:35:31 -0400

    init commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$('p').on('mouseover', function() {$(this).css({ "color": "red",}); });
+$('h2').on('mouseover', function() {
+  $(this).toggleClass('cool');
+});
+$('h2').on('mouseover', function() {
+  $(this).animate({
+    "letter-spacing": "10px",
+    "font-size": "40px"
+  }, 1000);
+});
+$('p').on('click', function() {
+  $(this).animate({
+    "height" : "0px",
+    "padding": "0px"
+  }, 400, function() {
+    $(this).html("");
+    alert("meowwwwww");
+  })
+});
